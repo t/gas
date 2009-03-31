299ef90ee832e58c334d5c454c52e7a8b085dc07
t <t@foo.jp>
2009-03-31 23:11:45 +0900

    remove some files

--- src/alt_precompute.h
+++
@@ -1,6 +0,0 @@
-#ifndef ALT_H_
-#define ALT_H_
-
-int alt_precompute(const std::string& db_dir, const std::string& strategy, const int& landmark_size);
-
-#endif
--- src/malt_precompute.h
+++
@@ -1,6 +0,0 @@
-#ifndef MALT_H_
-#define MALT_H_
-
-int malt_precompute(const std::string& db_dir, const std::string& strategy, const int& landmark_size);
-
-#endif
